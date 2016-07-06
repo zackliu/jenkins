36393250341857f800397b6d57b016615c35feca
zackliu <zackliu1995@hotmail.com>
2016-07-06 12:08:31 +0800

    add a job test

---
+++ dslJob.groovy
@@ -0,0 +1,11 @@
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
