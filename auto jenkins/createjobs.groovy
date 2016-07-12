866654609e2076ba5dbc588ca96f8062368e99d6
Chenyang Liu <t-chenl@microsoft.com>
2016-07-12 17:33:10 +0800

    update createjobs.groovy

--- auto jenkins/createjobs.groovy
+++ auto jenkins/createjobs.groovy
@@ -1,41 +1,55 @@
 import jenkins.model.Jenkins;
 import hudson.model.FreeStyleProject;
 import java.net.*
+import groovy.json.JsonSlurper
 
-oldSeedJob = Jenkins.instance.getItem("seed-job")
+def jsonPayload = new URL("https://raw.githubusercontent.com/zackliu/jenkins/master/config.json").getText();
+def state = new JsonSlurper().parseText(jsonPayload);
+
+
+/*oldSeedJob = Jenkins.instance.getItem("seed-job")
 if(oldSeedJob != null){
   oldSeedJob.delete();
 }
+*/
+
+def createSeedJobAndRun(def name, def url)
+{
+  job = Jenkins.instance.createProject(FreeStyleProject, name)
+  job.displayName = name
+
+  def text = new URL(url).getText()
 
 
-job = Jenkins.instance.createProject(FreeStyleProject, 'seed-job')
-job.displayName = 'Seed Job'
+  builder = new javaposse.jobdsl.plugin.ExecuteDslScripts(
+    new javaposse.jobdsl.plugin.ExecuteDslScripts.ScriptLocation(
+        'true',
+        null,
+        text,
+        ),
+    false,
+    javaposse.jobdsl.plugin.RemovedJobAction.DELETE, 
+    javaposse.jobdsl.plugin.RemovedViewAction.DELETE, 
+    javaposse.jobdsl.plugin.LookupStrategy.JENKINS_ROOT
+  )
+  job.buildersList.add(builder)
 
-def text = new URL("https://raw.githubusercontent.com/zackliu/jenkins/master/dslJob.groovy").getText()
+  job.save()
 
-
-builder = new javaposse.jobdsl.plugin.ExecuteDslScripts(
-  new javaposse.jobdsl.plugin.ExecuteDslScripts.ScriptLocation(
-      'true',
-    	null,
-      text,
-      ),
-  false,
-  javaposse.jobdsl.plugin.RemovedJobAction.DELETE, 
-  javaposse.jobdsl.plugin.RemovedViewAction.DELETE, 
-  javaposse.jobdsl.plugin.LookupStrategy.JENKINS_ROOT
-)
-job.buildersList.add(builder)
-
-job.save()
-
-job.getAllJobs().each{
-  run ->
-  if(run.getDisplayName() == 'Seed Job'){
-    run.scheduleBuild();
-   // run.delete(); 
+  job.getAllJobs().each{
+    run ->
+    if(run.getDisplayName() == name){
+      run.scheduleBuild();
+    // run.delete(); 
+    }
   }
 }
+
+state.seedJob.each{
+  createSeedJobAndRun(it.name, it.dslTemplateUrl);
+}
+
+
 
 
 
