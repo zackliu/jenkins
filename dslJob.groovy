123415e866292aeb6878eb345a0c16b28dcb8ac4
Chenyang Liu <t-chenl@microsoft.com>
2016-07-13 13:48:01 +0800

    new

--- dslJob.groovy
+++ dslJob.groovy
@@ -11,36 +11,38 @@ def getParameter(variable, specific, template)
     return null
 }
 
-
-
 def createCiJob(jobName, settings, codeBranch) {
+    def labelExpression = getParameter("labelExpression", settings.ci, settings.template)
+    def repoUrl = getParameter("repoUrl", settings.ci, settings.template)
+    def credentialId = getParameter("credentialId", settings.ci, settings.template)
+    def mailGroup = getParameter("mailGroup", settings.ci, settings.template)
     job(jobName) {
-        label(getParameter("labelExpression", settings.ci, settings.template))
+
+        label(labelExpression)
         logRotator { // Discard old builds
-            daysToKeep(getParameter("logRotator.daysToKeep", settings.ci, settings.template)) // If specified, build records are only kept up to this number of days.
-            numToKeep(getParameter("logRotator.numToKeep", settings.ci, settings.template)) // If specified, only up to this number of build records are kept.
-            artifactDaysToKeep(getParameter("logRotator.artifactDaysToKeep", settings.ci, settings.template)) // If specified, artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept.
-            artifactNumToKeep(getParameter("logRotator.artifactNumToKeep", settings.ci, settings.template)) // If specified, only up to this number of builds have their artifacts retained.
+            daysToKeep(settings.template.logRotator.daysToKeep) // If specified, build records are only kept up to this number of days.
+            numToKeep(settings.template.logRotator.numToKeep) // If specified, only up to this number of build records are kept.
+            artifactDaysToKeep(settings.template.logRotator.artifactDaysToKeep) // If specified, artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept.
+            artifactNumToKeep(settings.template.logRotator.artifactNumToKeep) // If specified, only up to this number of builds have their artifacts retained.
         }
+
         scm {
             git {
                 remote {
-                    url(jobSettings['repoUrl'])
-                    credentials(jobSettings['credentialId'])
+                    url(repoUrl)
+                    credentials(credentialId)
                 }
                 branch(codeBranch)
             }
         }
+
         steps {
-            shell([
-                'npm install',
-                'npm run opst init',
-                "npm run opst -- deployTheme -B ${codeBranch}"
-                ].join('\n'))
+            shell(settings.ci.shell.join('\n'))
         }
+
         publishers {
             extendedEmail {
-                recipientList(jobSettings['mailGroup'])
+                recipientList(mailGroup)
                 defaultSubject('$DEFAULT_SUBJECT')
                 defaultContent('$DEFAULT_CONTENT')
                 contentType('default')
@@ -56,6 +58,133 @@ def createCiJob(jobName, settings, codeBranch) {
         }
     }
 }
+
+def createE2eJob(jobName, upstreamJobName, settings, testBranch) {
+    def neast = testBranch + "_e2e"
+    def e2eSettings = settings.e2e."$neast"
+    def labelExpression = getParameter("labelExpression", settings.e2e, settings.template)
+    def repoUrl = getParameter("repoUrl", settings.e2e, settings.template)
+    def credentialId = getParameter("credentialId", settings.e2e, settings.template)
+    def mailGroup = getParameter("mailGroup", settings.e2e, settings.template)
+
+    job(jobName) {
+
+        label(labelExpression)
+
+        logRotator { // Discard old builds
+            daysToKeep(settings.template.logRotator.daysToKeep) // If specified, build records are only kept up to this number of days.
+            numToKeep(settings.template.logRotator.numToKeep) // If specified, only up to this number of build records are kept.
+            artifactDaysToKeep(settings.template.logRotator.artifactDaysToKeep) // If specified, artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept.
+            artifactNumToKeep(settings.template.logRotator.artifactNumToKeep) // If specified, only up to this number of builds have their artifacts retained.
+        }
+
+        triggers {
+            upstream(upstreamJobName, 'SUCCESS')
+        }
+
+        scm {
+            git {
+                remote {
+                    url(repoUrl)
+                    credentials(credentialId)
+                }
+                branch(e2eSettings.sourceBranch)
+            }
+        }
+
+        steps {
+            shell(settings.e2e.shell.join('\n'))
+        }
+
+        publishers {
+            artifactArchiver {
+                artifacts('result/e2e/screenshots/**/*.*') // Archive E2E test results
+            }
+            postBuildScripts {
+                onlyIfBuildSucceeds(false)
+                steps {
+                    batchFile('START taskkill /f /im chrome.exe \n' +
+                              'START taskkill /f /im chromedriver.exe')
+                }
+            }
+            extendedEmail {
+                recipientList(mailGroup)
+                defaultSubject('$DEFAULT_SUBJECT')
+                defaultContent('$DEFAULT_CONTENT')
+                contentType('default')
+                triggers {
+                    failure {
+                        sendTo {
+                            developers()
+                            recipientList()
+                        }
+                    }
+                    fixed {
+                        sendTo {
+                            developers()
+                            recipientList()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+def createMergeJob(jobName, upstreamJobName, settings, codeBranch, mergeTo) {
+    def labelExpression = getParameter("labelExpression", settings.merge, settings.template)
+    def repoUrl = getParameter("repoUrl", settings.merge, settings.template)
+    def parts = repoUrl.split(/[\/\\]/)
+    def repoName = parts[-1]
+    def credentialId = getParameter("credentialId", settings.merge, settings.template)
+    def mailGroup = getParameter("mailGroup", settings.merge, settings.template)
+
+    job(jobName) {
+        label(labelExpression)
+        logRotator { // Discard old builds
+            daysToKeep(settings.template.logRotator.daysToKeep) // If specified, build records are only kept up to this number of days.
+            numToKeep(settings.template.logRotator.numToKeep) // If specified, only up to this number of build records are kept.
+            artifactDaysToKeep(settings.template.logRotator.artifactDaysToKeep) // If specified, artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept.
+            artifactNumToKeep(settings.template.logRotator.artifactNumToKeep) // If specified, only up to this number of builds have their artifacts retained.
+        }
+        triggers {
+            upstream(upstreamJobName, 'SUCCESS')
+        }
+        scm {
+            git {
+                remote {
+                    name(repoName)
+                    url(repoUrl)
+                    credentials(credentialId)
+                }
+                branch(codeBranch)
+                extensions {
+                    mergeOptions {
+                        branch(mergeTo) // Sets the name of the branch to merge.
+                        remote(repoName) // Sets the name of the repository that contains the branch to merge.
+                    }
+                }
+            }
+        }
+        publishers {
+            git {
+                pushOnlyIfSuccess()
+                pushMerge()
+            }
+        }
+    }
+}
+
+
+def createOpbuildJob(jobName, upstreamJobName, templateJobSettings, e2eJobSettings, testBranch) {
+  
+}
+
+
+
+
+
 
 
 
